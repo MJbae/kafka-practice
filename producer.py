eab171581da55728d6359ff8a3d112c71ea93df8
MJbae <bmj5208@gmail.com>
2022-02-18 23:03:04 +0900

    fix: add flush in producer to send buffers

--- producer.py
+++ producer.py
@@ -2,4 +2,5 @@ import json
 from kafka import KafkaProducer
 
 producer = KafkaProducer(value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-producer.send('quickstart-events', {'foo': 'bar'})
+producer.send('quickstart-events', value={'foo': 'bar'})
+producer.flush()
