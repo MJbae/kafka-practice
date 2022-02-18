64e11de5ed866fb300157bce3c1625ced795a82c
maxwell <maxwell@aidkr.com>
2022-02-18 15:24:20 +0900

    feat: add 1st example of producer

--- producer.py
+++ producer.py
@@ -2,4 +2,4 @@ import json
 from kafka import KafkaProducer
 
 producer = KafkaProducer(value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-producer.send('fizzbuzz', {'foo': 'bar'})
+producer.send('quickstart-events', {'foo': 'bar'})
